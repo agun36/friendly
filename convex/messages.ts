9a8e23de19e096759c42e9a0136f3cb1a9f65438
agun36 <aguntoroti10@gmail.com>
2024-08-30 19:06:04 +0100

    first initialization

---
+++ convex/messages.ts
@@ -0,0 +1,20 @@
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+// read and order from the latest endpoingts
+export const list = query({
+  args: {},
+  handler: async (ctx) => {
+    // Grab the most recent messages.
+    const messages = await ctx.db.query("messages").order("desc").take(100);
+    return messages;
+  },
+});
+
+export const send = mutation({
+  args: { body: v.string(), author: v.string() },
+  handler: async (ctx, { body, author }) => {
+    // Send a new message.
+    await ctx.db.insert("messages", { body, author });
+  },
+});
